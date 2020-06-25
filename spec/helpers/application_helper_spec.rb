3277e640df6ed4bb02f16fb95bb54e8c4ddb255b
Mr. Marco Prins <snirpocram@gmail.com>
2020-06-25 20:54:52 +0200

    Fix description

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe ApplicationHelper do
   describe "#render_markdown" do
-    it "returns true" do
+    it 'converts markdown to html' do
       html = helper.render_markdown('This is a *test* for markdown rendering')
       expect(html).to eq("<p>This is a <em>test</em> for markdown rendering</p>\n")
     end
