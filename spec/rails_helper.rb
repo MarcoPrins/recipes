e8937b097eef4d902accce97da74b794b1bd61c0
Mr. Marco Prins <snirpocram@gmail.com>
2020-06-25 18:35:52 +0200

    Use VCR

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,15 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data("<access-token>") do
+    ENV['CONTENTFUL_ACCESS_TOKEN']
+  end
+end
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -10,6 +19,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
