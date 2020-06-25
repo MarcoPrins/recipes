63541b2dfc9bcf9d61132cbc58bbe80f446d9076
Mr. Marco Prins <snirpocram@gmail.com>
2020-06-25 20:24:35 +0200

    Expect 200 in controller specs

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -8,6 +8,7 @@ describe RecipesController, type: :controller do
       VCR.use_cassette("recipes/all") do
         expect(Contentful::Recipe).to(receive(:all)).and_call_original
         get :index
+        expect(response.status).to eq(200)
       end
     end
   end
@@ -17,6 +18,7 @@ describe RecipesController, type: :controller do
       VCR.use_cassette("recipes/find") do
         expect(Contentful::Recipe).to(receive(:find).with('test-id')).and_call_original
         get :show, params: { id: 'test-id' }
+        expect(response.status).to eq(200)
       end
     end
   end
